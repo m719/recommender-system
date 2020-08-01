a5bd42c60dde1e55c6ddb4a47690c2767429c32b
Murat Koc <mkmac@MacBook-Air-de-Murat.local>
2020-08-01 21:36:42 +0200

    Added all origins CORS

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ mongoose.connect(config.databaseUrl);
 
 // Handles CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
